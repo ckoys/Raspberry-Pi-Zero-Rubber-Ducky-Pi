03b74f6476c6a3f3d35a4d01c66750d865f9c499
Lukas Bonrath <51073652+lucki1000@users.noreply.github.com>
2019-05-27 16:58:53 +0200

    Update duckysetup.sh

--- duckysetup.sh
+++ duckysetup.sh
@@ -4,9 +4,9 @@ if [ $EUID -ne 0 ]; then
 	echo "sudo $0 $@"
 	exit
 fi
-apt-get update
-apt-get upgrade -y
-apt-get install rpi-update
+apt update
+apt upgrade -y
+apt install rpi-update
 BRANCH=next rpi-update c053625
 
 ## dwc2 drivers
@@ -19,7 +19,7 @@ wget --no-check-certificate https://raw.githubusercontent.com/ossiozac/Raspberry
 cd /home/pi
 chmod 755 hid-gadget-test.c duckpi.sh usleep.c g_hid.ko usleep hid-gadget-test
 
-\cp g_hid.ko /lib/modules/4.4.0+/kernel/drivers/usb/gadget/legacy
+\cp g_hid.ko /lib/modules/4.*.*+/kernel/drivers/usb/gadget/legacy
 
 cat <<'EOF'>>/etc/modules
 dwc2
@@ -27,6 +27,7 @@ g_hid
 EOF
 
 ##Make it so that you can put the payload.dd in the /boot directory
+sudo chmod +x /usr/bin/hid_usb
 sed -i '/exit/d' /etc/rc.local
 
 cat <<'EOF'>>/etc/rc.local
@@ -36,6 +37,7 @@ sleep 1
 tr -d '\r' < /home/pi/payload.dd > /home/pi/payload2.dd
 sleep 1
 /home/pi/duckpi.sh /home/pi/payload2.dd
+/usr/bin/hid_usb # libcomposite configuration
 exit 0
 EOF
 
