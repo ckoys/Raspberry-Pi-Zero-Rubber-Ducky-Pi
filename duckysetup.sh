4b9becc06942b7bbaee2ceef33283b3e1e7ba542
Lukas Bonrath <51073652+lucki1000@users.noreply.github.com>
2019-06-22 22:17:15 +0200

    If statement not working :(

--- duckysetup.sh
+++ duckysetup.sh
@@ -19,6 +19,38 @@ sudo echo "libcomposite" | sudo tee -a /etc/modules
 ##Install git and download rspiducky
 wget --no-check-certificate https://raw.githubusercontent.com/lucki1000/Raspberry-Pi-Zero-Rubber-Ducky-Duckberry-Pi/master/LICENSE https://raw.githubusercontent.com/lucki1000/Raspberry-Pi-Zero-Rubber-Ducky-Duckberry-Pi/master/duckpi.sh https://raw.githubusercontent.com/lucki1000/Raspberry-Pi-Zero-Rubber-Ducky-Duckberry-Pi/master/g_hid.ko https://raw.githubusercontent.com/lucki1000/Raspberry-Pi-Zero-Rubber-Ducky-Duckberry-Pi/master/hid-gadget-test https://raw.githubusercontent.com/lucki1000/Raspberry-Pi-Zero-Rubber-Ducky-Duckberry-Pi/master/hid-gadget-test.c https://raw.githubusercontent.com/lucki1000/Raspberry-Pi-Zero-Rubber-Ducky-Duckberry-Pi/master/hid_usb https://raw.githubusercontent.com/lucki1000/Raspberry-Pi-Zero-Rubber-Ducky-Duckberry-Pi/master/readme.md https://raw.githubusercontent.com/lucki1000/Raspberry-Pi-Zero-Rubber-Ducky-Duckberry-Pi/master/usleep https://raw.githubusercontent.com/lucki1000/Raspberry-Pi-Zero-Rubber-Ducky-Duckberry-Pi/master/usleep.c https://raw.githubusercontent.com/lucki1000/Raspberry-Pi-Zero-Rubber-Ducky-Pi/test/duckpi_de.sh
 
+if [ $layout == "us" ] 
+then
+	sed -i '/exit/d' /etc/rc.local
+	cat <<\EOF >>/etc/rc.local
+	/usr/bin/hid_usb # libcomposite configuration
+	sleep 3
+	cat /boot/payload.dd > /home/pi/payload.dd
+	sleep 1
+	tr -d '\r' < /home/pi/payload.dd > /home/pi/payload2.dd
+	sleep 1
+	/home/pi/duckpi.sh /home/pi/payload2.dd
+	exit 0
+	\EOF
+	gcc /home/pi/hid-gadget-test.c -o /home/pi/hid-gadget-test
+fi
+
+if [ $layout == "de"]
+then
+	sed -i '/exit/d' /etc/rc.local
+	cat <<\EOF >>/etc/rc.local
+	/usr/bin/hid_usb # libcomposite configuration
+	sleep 3
+	cat /boot/payload.dd > /home/pi/payload.dd
+	sleep 1
+	tr -d '\r' < /home/pi/payload.dd > /home/pi/payload2.dd
+	sleep 1
+	/home/pi/duckpi_german_layout.sh /home/pi/payload2.dd
+	exit 0
+	\EOF
+	gcc /home/pi/hid-gadget-test_german.c -o /home/pi/hid-gadget-test
+fi
+
 ##Make all nessisary files executeable
 cd /home/pi
 chmod 755 hid-gadget-test.c duckpi.sh usleep.c g_hid.ko usleep hid-gadget-test
@@ -33,39 +65,6 @@ EOF
 ##Make it so that you can put the payload.dd in the /boot directory
 sudo cp -r /home/pi/hid_usb /usr/bin/hid_usb
 sudo chmod +x /usr/bin/hid_usb
-
-if [ $layout == "us" ] 
-then
-	sed -i '/exit/d' /etc/rc.local
-	cat <<'EOF'>>/etc/rc.local
-	/usr/bin/hid_usb # libcomposite configuration
-	sleep 3
-	cat /boot/payload.dd > /home/pi/payload.dd
-	sleep 1
-	tr -d '\r' < /home/pi/payload.dd > /home/pi/payload2.dd
-	sleep 1
-	/home/pi/duckpi.sh /home/pi/payload2.dd
-	exit 0
-	EOF
-	gcc /home/pi/hid-gadget-test.c -o /home/pi/hid-gadget-test
-fi
-
-if [ $layout == "de"]
-then
-	sed -i '/exit/d' /etc/rc.local
-	cat <<'EOF'>>/etc/rc.local
-	/usr/bin/hid_usb # libcomposite configuration
-	sleep 3
-	cat /boot/payload.dd > /home/pi/payload.dd
-	sleep 1
-	tr -d '\r' < /home/pi/payload.dd > /home/pi/payload2.dd
-	sleep 1
-	/home/pi/duckpi_german_layout.sh /home/pi/payload2.dd
-	exit 0
-	EOF
-	gcc /home/pi/hid-gadget-test_german.c -o /home/pi/hid-gadget-test
-fi
-
 
 ##Making the first payload
 cat <<'EOF'>>/boot/payload.dd
